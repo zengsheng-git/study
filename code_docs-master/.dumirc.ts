0c44f1214ba0a7d020e9b160cea127d801894d80
sheng.zeng <1218128305@qq.com>
2023-07-31 15:54:53 +0800

    修改打包后的路径配置

--- code_docs-master/.dumirc.ts
+++ code_docs-master/.dumirc.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist', // 打包之后的文件夹
+  outputPath: 'dist', // 打包之后生成的文件夹
   themeConfig: {
     name: '编程文档',
-    logo: "/code_docs/logo.webp",
+    logo: "/logo.webp",  // logo路劲
     footer: "“无人问津也好，技不如人也罢，你都要试着安静下来，去做自己该做的事，而不是让内心的烦躁焦虑毁掉你本就不多的热情和定力” ——人民日报"
   },
-  favicons: ['/code_docs/logo.ico'],
+  favicons: ['/logo.ico'], // icon路劲
   history: {
     type: "hash" // hash模式
   },
@@ -16,5 +16,5 @@ export default defineConfig({
     baidu: '3ea846ff08b64301f51a1bd92e3a5019',
   },
   base: "/", // 文档起始路由
-  publicPath: "/code_docs/" // 静态资源起始路径
+  publicPath: "/" // 静态资源起始路径
 });
